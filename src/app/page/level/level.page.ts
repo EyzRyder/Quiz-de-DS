edc899a1f5bd66a0dae50b3ca4115bc9541898ea
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-05-07 14:50:25 -0300

    remove functions

--- src/app/page/level/level.page.ts
+++ src/app/page/level/level.page.ts
@@ -13,33 +13,33 @@ export class LevelPage implements OnInit {
   ngOnInit() {
   }
   q1(){
-    this.router.navigate(['etim']);
+
   }
   q2(){
-    this.router.navigate(['etim']);
+
   }
   q3(){
-    this.router.navigate(['etim']);
+
   }
   q4(){
-    this.router.navigate(['etim']);
+
   }
   q5(){
-    this.router.navigate(['etim']);
+
   }
   q6(){
-    this.router.navigate(['etim']);
+
   }
   q7(){
-    this.router.navigate(['etim']);
+
   }
   q8(){
-    this.router.navigate(['etim']);
+
   }
   q9(){
-    this.router.navigate(['etim']);
+
   }
   q10(){
-    this.router.navigate(['etim']);
+
   }
 }
